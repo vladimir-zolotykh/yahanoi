86b4423d7483d2f94bb338304e5296eb15cb61b0
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-12 10:46:53 +0300

    Initial import

---
+++ hanoi.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import pytest
+
+
+class Board:
+    peg1 = []
+    peg2 = []
+    peg3 = []
+
+
+def init_peg(num_disks):
+    return list(range(num_disks, 0, -1))
+
+
+# $ pytest hanoi.py::test_init_peg
+@pytest.mark.parametrize(
+    "num_disks, peg",
+    [
+        (1, [1]),
+        (2, [2, 1]),
+        (3, [3, 2, 1]),
+        (4, [4, 3, 2, 1]),
+        (5, [5, 4, 3, 2, 1]),
+        (6, [6, 5, 4, 3, 2, 1]),
+        (7, [7, 6, 5, 4, 3, 2, 1]),
+        (8, [8, 7, 6, 5, 4, 3, 2, 1]),
+    ],
+)
+def test_init_peg(num_disks, peg):
+    assert init_peg(num_disks) == peg
+
+
+def move(peg1, peg2, peg3):
+    peg2.append(peg1.pop())
+
+
+def solve(peg1, peg2, peg3):
+    pass
