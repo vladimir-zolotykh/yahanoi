cb10250e2560733cf3a9dc50384557ee63f46228
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-13 10:30:58 +0300

    Refactor test_tuple()
    Make it pytest

--- tuple.py
+++ tuple.py
@@ -2,17 +2,20 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 from operator import itemgetter
+import pytest
 
 
-def TupleMeta(type):
+class TupleMeta(type):
     def __init__(cls, clsname, bases, clsdict):
-        super().__init__(cls, clsname, bases, clsdict)
+        super().__init__(clsname, bases, clsdict)
         fields = clsdict.get("_fields", [])
         for i, name in enumerate(fields):
             setattr(cls, "name", property(itemgetter(i)))
 
 
-def Tuple(tuple, metaclass=TupleMeta):
+class Tuple(tuple, metaclass=TupleMeta):
+    _fields: list[str] = []
+
     def __new__(cls, *args, **kwargs):
         n = len(cls._fields)
         if len(args) != n:
@@ -26,7 +29,11 @@ class Exercise(Tuple):
 
 def test_tuple():
     e = Exercise("squat", 77.5, 2)
-    print(e)
+    assert str(e) == "('squat', 77.5, 2)"
+    with pytest.raises(TypeError, match="<class 'Exercise'>: gets exactly 3 arguments"):
+        Exercise("squat", 77.5)
+    with pytest.raises(TypeError, match="<class 'Exercise'>: gets exactly 3 arguments"):
+        Exercise("squat", 77.5, 3, "failed")
 
 
 if __name__ == "__main__":
