e39e03494f55f6b42230719e23ba7bf0b6184111
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-16 10:35:33 +0300

    Initial import
    Add BunchMeta, Person, test_person(), main guard

---
+++ bunch.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class BunchMeta(type):
+    def __new__(mcls, clsname, bases, clsdict):
+        defaults = {}
+
+        def init(self, **kwargs):
+            for key, val in defaults.items():
+                setattr(self, key, kwargs[key] if key in kwargs else val)
+
+        def repr(self):
+            csv = ", ".join(
+                f"{key}={val2}"
+                for key, val in defaults.items()
+                if (val2 := getattr(self, key)) != val
+            )
+            return f"{type(self).__name__}({csv})"
+
+        for key, val in clsdict.items():
+            if key[:2] == "__" and key[-2:] == "__":
+                if key in ("__init__", "__repr__"):
+                    raise TypeError(f"Cannot overwrite {key!r}")
+                continue
+            defaults[key] = val
+        clsdict["__slots__"] = ["name", "age", "pension"]
+        clsdict["__init__"] = init
+        clsdict["__repr__"] = repr
+
+
+class Person(metaclass=BunchMeta):
+    name = "Vladimir"
+    age = 61
+    pension = 4606.93
+
+
+def test_person():
+    p = Person()
+    print(p)
+
+
+if __name__ == "__main__":
+    test_person()
