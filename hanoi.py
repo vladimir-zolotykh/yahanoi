06eccb707f60014be2de7f0672f452cf1c724093
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-16 21:51:16 +0300

    Refactor  num_disks == 3 branch
        elif num_disks == 3:

--- hanoi.py
+++ hanoi.py
@@ -73,14 +73,8 @@ def solve(peg: PegsType, num_disks: int, src: int, dst: int) -> None:
         move(peg, 2, 3)  # aux: 1
         assert solvedp(peg, num_disks)
     elif num_disks == 3:
-        # move(peg, 1, 3)  # aux: 2
-        # move(peg, 1, 2)  # aux: 3
-        # move(peg, 3, 2)  # aux: 1
         stack2(peg, 1, 2, 3)
-        move(peg, 1, 3)  # aux: 2
-        # move(peg, 2, 1)  # aux: 3
-        # move(peg, 2, 3)  # aux: 1
-        # move(peg, 1, 3)  # aux: 2
+        move(peg, 1, 3)
         stack2(peg, 2, 3, 1)
         assert solvedp(peg, num_disks)
     elif num_disks == 4:
