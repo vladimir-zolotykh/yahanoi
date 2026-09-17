b2a4ada02353e6bf0a81382e8712aa9e1ac4937e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-17 18:51:14 +0300

    Fix, add __eq__, from_file
    Add __eq__ to Point, Box, Header
    Add from_file to Point, Box
    Fix read_polys()
    -                polys.append(poly)
    +            polys.append(poly)

--- polygons/write_polys.py
+++ polygons/write_polys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Self
 from itertools import chain
 import struct
 import io
@@ -17,26 +18,46 @@ POLYS: PolysType = [
 
 
 class Point:
-    @typechecked
+    # @typechecked
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        else:
+            return NotImplemented
+
     def packed(self):
         return struct.pack("<dd", self.x, self.y)
 
+    @classmethod
+    def from_file(cls, f) -> Self:
+        return cls(*struct.unpack("<dd", f.read(struct.calcsize("<dd"))))
+
 
 class Box:
-    @typechecked
+    # @typechecked
     def __init__(self, p1: Point, p2: Point):
         self.p1 = p1
         self.p2 = p2
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Box):
+            return self.p1 == other.p1 and self.p2 == other.p2
+        else:
+            return NotImplemented
 
     def packed(self):
         with io.BytesIO() as f:
             f.write(self.p1.packed())
             f.write(self.p2.packed())
             return f.getvalue()
+
+    @classmethod
+    def from_file(cls, f) -> Self:
+        return cls(Point.from_file(f), Point.from_file(f))
 
 
 def get_bounding_box(polys: PolysType = POLYS) -> Box:
@@ -47,11 +68,17 @@ def get_bounding_box(polys: PolysType = POLYS) -> Box:
 
 
 class Header:
-    @typechecked
+    # @typechecked
     def __init__(self, magic: int, box: Box, num_polys: int):
         self.magic = magic
         self.box = box
         self.num_polys = num_polys
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Header):
+            return self.magic == other.magic
+        else:
+            return NotImplemented
 
     def packed(self) -> bytes:
         with io.BytesIO() as f:
@@ -64,6 +91,14 @@ class Header:
     def default(cls):
         return cls(0x1234, get_bounding_box(POLYS), len(POLYS))
 
+    @classmethod
+    def from_file(cls, f) -> Self:
+        return cls(
+            struct.unpack("<i", f.read(struct.calcsize("<i")))[0],
+            Box.from_file(f),
+            struct.unpack("<i", f.read(struct.calcsize("<i")))[0],
+        )
+
 
 _POLYS_BIN = ".polys.bin"
 
@@ -71,11 +106,12 @@ _POLYS_BIN = ".polys.bin"
 def write_polys(filename: str = _POLYS_BIN, polys: PolysType = POLYS):
     header = Header.default()
     with open(filename, "wb") as f:
-        header.packed()
-        for poly in polys:
-            f.write(struct.pack("<i", len(poly)))
-            for point in poly:
-                f.write(struct.pack("<dd", *point))
+        with open(_POLYS_BIN, "wb") as f:
+            f.write(header.packed())
+            for poly in polys:
+                f.write(struct.pack("<i", len(poly)))
+                for point in poly:
+                    f.write(struct.pack("<dd", *point))
 
 
 def read_polys(filename: str = _POLYS_BIN) -> PolysType:
@@ -87,9 +123,15 @@ def read_polys(filename: str = _POLYS_BIN) -> PolysType:
             poly: _PolyType = []
             for _ in range(num_points):
                 poly.append(struct.unpack("<dd", f.read(struct.calcsize("<dd"))))
-                polys.append(poly)
+            polys.append(poly)
     return polys
 
 
 if __name__ == "__main__":
     write_polys()
+    header = Header.default()
+    with open(_POLYS_BIN, "rb") as f:
+        header2 = Header.from_file(f)
+    assert header == header2
+    polys2 = read_polys()
+    assert polys2 == POLYS
