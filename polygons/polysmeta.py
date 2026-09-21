bff42fb79054682503d48def9c42eda2cec1a79a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-21 10:42:02 +0300

    Add init_as()
    Refactor FieldType.fetch(), iter_as(), main guard with init_as

--- polygons/polysmeta.py
+++ polygons/polysmeta.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Self, BinaryIO
+from typing import Self, BinaryIO, TypeVar
 from abc import ABC, abstractmethod
 from functools import partial
 from operator import attrgetter
@@ -9,7 +9,15 @@ import struct
 import argparse
 import argcomplete
 
+T = TypeVar("T")
 # import write_polys as WP
+
+
+def init_as(init_type: type, real_type: type[T], data: memoryview | bytes) -> T:
+    """Create real_type class, init it as init_type"""
+    obj = real_type.__new__(real_type)
+    init_type.__init__(obj, data)
+    return obj
 
 
 class Field(ABC):
@@ -54,8 +62,9 @@ class FieldType(Field):
 
     def fetch(self, instance):
         rng = slice(self.off, self.off + self.type_._type_size)
-        obj = self.type_.__new__(self.type_)
-        View.__init__(obj, instance._data[rng])
+        # obj = self.type_.__new__(self.type_)
+        # View.__init__(obj, instance._data[rng])
+        obj = init_as(View, self.type_, instance._data[rng])
         # return self.type_(instance._data[rng])
         return obj
 
@@ -161,8 +170,9 @@ class Sized:
         for off in range(0, len(self.data), _size(fmt_or_type)):
             lump = slice(off, off + _size(fmt_or_type))
             if isinstance(_factory, type):
-                obj = _factory.__new__(_factory)
-                View.__init__(obj, self.data[lump])
+                # obj = _factory.__new__(_factory)
+                # View.__init__(obj, self.data[lump])
+                obj = init_as(View, _factory, self.data[lump])
                 yield obj
             else:
                 yield _factory(self.data[lump])
@@ -181,8 +191,9 @@ if __name__ == "__main__":
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
     with open(_POLYS_BIN, "rb") as fd:
-        hdr = Header.__new__(Header)
-        View.__init__(hdr, fd.read(Header._type_size))
+        # hdr = Header.__new__(Header)
+        # View.__init__(hdr, fd.read(Header._type_size))
+        hdr = init_as(View, Header, fd.read(Header._type_size))
         print(hdr.csv())
         for _ in range(hdr.num_polys):
             polys = Sized.from_file(fd)
