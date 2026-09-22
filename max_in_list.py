30fc14fb768e0a991066364df357aabfce84e77e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-22 09:46:27 +0300

    Fix binary_search(), add test_binary_search_not_found()

--- max_in_list.py
+++ max_in_list.py
@@ -57,25 +57,37 @@ def test_flatten():
     assert flat == [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-def binary_search(data: list[int], target: int, low: int = -1, high: int = -1):
-    if low < 0:
-        low = 0
-    if high < 0:
+def binary_search(
+    data: list[int], target: int, low: int = 0, high: int | None = None
+) -> int | None:
+    if high is None:
         high = len(data)
     if low >= high:
-        return low
-    mid = (high - low) // 2
+        return None
+    mid = (high + low) // 2
     if target == data[mid]:
         return mid
     elif target < data[mid]:
-        return binary_search(data, target, low, mid)
+        return binary_search(data, target, low, mid - 1)
     else:
-        return binary_search(data, target, mid, high)
+        return binary_search(data, target, mid + 1, high)
 
 
-def test_binary_search():
-    data = [1, 2, 3, 4, 5, 6, 7, 8]
-    assert binary_search(data, 5) == 4
+@pytest.mark.parametrize(
+    "dat, tgt",
+    [
+        ([1, 2, 3, 4, 5, 6, 7, 8], 5),
+        ([1, 31, 37, 60, 67, 72, 73, 83, 89, 91], 73),
+        ([1, 31, 37, 60, 67, 72, 73, 83, 89, 91], 37),
+    ],
+)
+def test_binary_search(dat, tgt):
+    assert binary_search(dat, tgt) == dat.index(tgt)
+
+
+def test_binary_search_not_found():
+    dat = [1, 31, 37, 60, 67, 72, 73, 83, 89, 91]
+    assert binary_search(dat, 38) is None
 
 
 if __name__ == "__main__":
@@ -83,3 +95,4 @@ if __name__ == "__main__":
     test_is_palindrome()
     test_flatten()
     test_binary_search()
+    test_binary_search_not_found()
