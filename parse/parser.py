fb23bbd1eebfa0450df4664817fa890ccb85a1d9
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-23 10:51:45 +0300

    Initial import
    Add Parser class, main guard to call Parser().parse()

---
+++ parse/parser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from itertokens import iter_tokens, Token
+from node import Node, Num, Plus, Minus, Mul, Div
+
+
+class Parser:
+    def __init__(self):
+        self.tokens = None
+        self.tok = None
+
+    def expr(self):
+        res: Node = self.term()
+        while self.tok and self.tok.val in ("+", "-"):
+            self._consume()
+            right: Node = self.term()
+            if self.tok.val == "+":
+                res = Plus(res, right)
+            else:
+                res = Minus(res, right)
+        return res
+
+    def term(self) -> Node:
+        res: Node = self.factor()
+        while self.tok and self.tok.val in ("*", "/"):
+            self._consume()
+            right: Node = self.factor()
+            if self.tok.val == "*":
+                res = Mul(res, right)
+            else:
+                res = Div(res, right)
+        return res
+
+    def factor(self) -> Node:
+        if self.tok.name == "LPAREN":
+            self._consume()
+            res = self.expr()
+            self._expect("RPAREN")
+        else:
+            res = Num(float(self.tok.val))
+            self._consume()
+        return res
+
+    def parse(self, sexpr: str) -> Node:
+        self.tokens = iter_tokens(sexpr)
+        self._advance()
+        return self.expr()
+
+    def _advance(self) -> Token:
+        self.tok = next(self.tokens, None)
+        return self.tok
+
+    def _consume(self) -> None:
+        self.tok = next(self.tokens, None)
+
+    def _expect(self, expected: Token) -> None:
+        if self.tok.name != expected.name:
+            raise SyntaxError(f"{self.tok}: expected {expected}")
+        self._consume()
+
+
+if __name__ == "__main__":
+    sexpr = "2 + (3 * 4) + 5"
+    p = Parser()
+    p.parse(sexpr)
+    print(p)
