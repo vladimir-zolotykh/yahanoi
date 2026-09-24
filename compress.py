f0c6f286a99446e2fa393ea9bf435eb06170aa28
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-24 09:00:58 +0300

    Draft compress

--- compress.py
+++ compress.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Any
 import pytest
+
+
+def compress(rest, chunk=None, result=[]) -> list[Any]:
+    if not rest:
+        return result + (chunk[1:] if chunk else [])
+    x = rest[0]
+    print(f"{chunk = }")
+    if chunk:
+        if chunk[0] == x:
+            chunk[1] += 1
+        else:
+            if chunk[1] > 1:
+                result = result + list(chunk)
+            else:
+                result = result + list(chunk[0])
+                result = result + list(x)
+                chunk = None
+    else:
+        chunk = [x, 1]
+    return compress(rest[1:], chunk, result)
 
 
 def sum_args(*args: int) -> int:
