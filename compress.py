1da992a4886638e3044ac6fe0b41f032aa0c0961
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-24 13:22:03 +0300

    Refactor compress()
    Add cat, count_ones, sum_args

--- compress.py
+++ compress.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Any
 import pytest
 
 
-def compress(rest, chunk=None, result=[]) -> list[Any]:
-    if not rest:
-        return result + (chunk[1:] if chunk else [])
-    x = rest[0]
-    print(f"{chunk = }")
-    if chunk:
-        if chunk[0] == x:
-            chunk[1] += 1
-        else:
-            if chunk[1] > 1:
-                result = result + list(chunk)
-            else:
-                result = result + list(chunk[0])
-                result = result + list(x)
-                chunk = None
+def cat(lst, chunk):
+    if not lst and not chunk:
+        return []
+    if lst and not chunk:
+        return lst
+    if lst and chunk:
+        return lst + [chunk]
+
+
+def replace_chunks(lst, chunk=[], res=[]):
+    if not lst:
+        return cat(res, chunk)
+    if not chunk:
+        chunk = [None, 0]
+    x = lst[0]
+    if chunk[0] == x:
+        chunk[1] += 1
+        return replace_chunks(lst[1:], chunk, res)
     else:
-        chunk = [x, 1]
-    return compress(rest[1:], chunk, result)
+        return replace_chunks(lst[1:], None, cat(res, chunk))
+
+
+def count_ones(lst: list[int], count: int = 0) -> int:
+    if not lst:
+        return count
+    if lst[0] == 1:
+        count += 1
+    return count_ones(lst[1:], count)
 
 
 def sum_args(*args: int) -> int:
     if not args:
         return 0
     return sum_args(*args[1:]) + args[0]
-
-
-@pytest.mark.parametrize(
-    "args, sum",
-    [
-        ((), 0),
-        ((1,), 1),
-        ((1, 2, 3), 6),
-    ],
-)
-def test_sum_args(args, sum):
-    assert sum_args(*args) == sum
