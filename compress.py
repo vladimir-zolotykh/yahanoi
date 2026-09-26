c7c7a716740a21f39029ae547d72937557491cb4
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-26 08:57:00 +0300

    Refactor join_chunks with Chunk(NamedList)

--- compress.py
+++ compress.py
@@ -19,11 +19,18 @@ class NamedList(list):
             )
 
 
-def jchunk1(all_chunks, chunk):
-    if chunk[0] > 1:
+class Chunk(NamedList):
+    def __init__(self, cnt: int, val: int):
+        super().__init__(("cnt", "val"), (cnt, val))
+
+
+def jchunk1(all_chunks: list[int | Chunk], chunk: Chunk):
+    # if chunk[0] > 1:
+    if chunk.cnt > 1:
         return all_chunks + [chunk]
     else:
-        return all_chunks + [chunk[1]]
+        # return all_chunks + [chunk[1]]
+        return all_chunks + [chunk.val]
 
 
 def join_chunks(
@@ -33,12 +40,15 @@ def join_chunks(
         return jchunk1(all_chunks, chunk)
     elt = lst[0]
     if chunk == []:
-        chunk = [1, elt]
+        # chunk = [1, elt]
+        chunk = Chunk(1, elt)
     elif chunk[1] == elt:
-        chunk[0] += 1
+        # chunk[0] += 1
+        chunk.cnt += 1
     else:
         all_chunks = jchunk1(all_chunks, chunk)
-        chunk = [1, elt]
+        # chunk = [1, elt]
+        chunk = Chunk(1, elt)
     return join_chunks(lst[1:], chunk, all_chunks)
 
 
